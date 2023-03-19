6edda3995844b94971575f6767b256cd50378834
calebhuangsea <calebhuangsea@gmail.com>
2023-03-19 00:01:24 -0700

    M33 and E704

---
+++ leetcode/src/E704BinarySearch/Solution.java
@@ -0,0 +1,27 @@
+package E704BinarySearch;
+
+/**
+ * Just Binary Search
+ */
+public class Solution {
+    /**
+     * Binary Search
+     * Time Complexity: O(Nlog(N)) 0ms 100%
+     * Space Complexity: O(1) 43.3MB 26.3%
+     */
+    public int search(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid;
+            }
+        }
+        return -1;
+    }
+}
---
+++ leetcode/src/M33SearchInRotatedSortedArray/Solution.java
@@ -0,0 +1,35 @@
+package M33SearchInRotatedSortedArray;
+
+/**
+ * Search a value in a rotated array
+ */
+public class Solution {
+    /**
+     * Binary Search with extra lo and hi check
+     * Time Complexity: O(Nlog(N)) 0ms 100%
+     * Space Complxity: O(1) 41.8MB 71.56%
+     */
+    public int search(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < nums[lo]) {// this means there is a cut at the left
+                if (target <= nums[hi] && target > nums[mid]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            } else {// this means there is a cut at the right
+                if (target >= nums[lo] && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
