f898f7448a2748a4906812a52c898827ae821300
calebhuangsea <calebhuangsea@gmail.com>
2023-03-20 16:11:56 -0700

    03/20/2023

---
+++ leetcode/src/M133CloneGraph/Solution.java
@@ -0,0 +1,47 @@
+package M133CloneGraph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Recursively find neighbor and add neighbor, use a map to keep track of repetitive node
+ * to avoid infinite loop and duplicate new node.
+ * Time Complexity O(N) 26ms 49.53%, visit every node
+ * Space Complexity O(N) 42MB 99.4, use a map to store visited nodes.
+ */
+public class Solution {
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    Map<Integer, Node> map = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (map.containsKey(node.val)) {
+            return map.get(node.val);
+        }
+        Node newNode = new Node(node.val, new ArrayList<>());
+        map.put(node.val, newNode);
+        for (Node neigh : node.neighbors) {
+            newNode.neighbors.add(cloneGraph(neigh));
+        }
+        return newNode;
+    }
+}
